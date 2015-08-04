81f0bffcf6192cdd658e099c9aa4d91428e48b52
jdfeehs <feehsjd@dukes.jmu.edu>
2015-08-03 21:38:48 -0400

    Finished problem2 of set1: fixed XOR

--- set1/set1.go
+++ set1/set1.go
@@ -10,15 +10,42 @@ package set1
 import (
     "encoding/hex"
     "encoding/base64"
+	"errors"
 )
 
 
-
-func HexTo64(str string) string {
+//Challenge 1
+func HexTo64(str string) (string,error) {
     bytes,err := hex.DecodeString(str)
     if err != nil {
-        return err.Error()
+        return "",err
     }
     ret := base64.StdEncoding.EncodeToString(bytes)    
-    return ret
+    return ret,nil
 }
+
+//Challenge 2
+func FixedXOR(buf1 string, buf2 string) (string,error) {
+	//Convert the strings to bytes, thne error check
+	hex1,err1 := hex.DecodeString(buf1)
+	hex2,err2 := hex.DecodeString(buf2)
+
+	if err1 != nil{
+		return "", err1
+	}
+	if err2 != nil{
+		return "", err2
+	}
+	if len(hex1) != len(hex2){
+		return "",errors.New("String lengths not equal")
+	}
+
+	for i := range(hex1){
+		hex2[i] ^= hex1[i]
+	}
+	return hex.EncodeToString(hex2),nil
+
+}
+
+
+
--- set1_main/set1_main.go
+++ set1_main/set1_main.go
@@ -8,7 +8,29 @@ import(
 
 
 func main() {
+
+	fmt.Println("-------------------------------------")
+	fmt.Println("Test 1")
+	fmt.Println("-------------------------------------")
     str := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    ret1 := set1.HexTo64(str)
-    fmt.Println(ret1)
+    ret1,err1 := set1.HexTo64(str)
+
+    if err1 == nil{
+		fmt.Println(ret1)
+	} else {
+		fmt.Println(err1)
+	}
+
+	fmt.Println("-------------------------------------")
+	fmt.Println("Test 2")
+	fmt.Println("-------------------------------------")
+	str1 := "1c0111001f010100061a024b53535009181c"
+	str2 := "686974207468652062756c6c277320657965"
+
+	ret2,err2 := set1.FixedXOR(str1,str2)
+	if err2 == nil{
+		fmt.Println(ret2)
+	} else {
+		fmt.Println(err2)
+	}
 }
